82c64d408e4f1568c84c8731871ed8d6d85d87d5
Thành Phát <thanhphat19@gmail.com>
2022-04-06 16:06:05 +0700

    remove console.log

--- src/redux/actions/AuthAction.js
+++ src/redux/actions/AuthAction.js
@@ -1,19 +1,16 @@
 import { authService } from "../../services/AuthService"
 import { LOGIN } from "../constants/AuthConstant";
-// import { history } from '../../App'
 
 export const LoginAction = (dataLogin, history) => {
-  console.log("🚀 ~ file: AuthAction.js ~ line 4 ~ loginAction ~ data", dataLogin)
   return async (dispatch) => {
     try {
       const {data, status} = await authService.login(dataLogin);
-      console.log("🚀 ~ file: AuthAction.js ~ line 9 ~ return ~ status", status)
       if (status === 200) {
         dispatch({
           type: LOGIN,
           data
         })
-        history.goBack();
+        history.push('/admin/dashboard');
       }
     } catch (err) {
       console.log("error", err);
