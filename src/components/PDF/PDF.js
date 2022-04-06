db634f4ac1536bf8ba1b84c9005faeff8bdf0108
Thành Phát <thanhphat19@gmail.com>
2022-04-06 17:00:26 +0700

    fix bug

--- src/components/PDF/PDF.js
+++ src/components/PDF/PDF.js
@@ -8,8 +8,6 @@ export default function PDF(props) {
     console.log("🚀 ~ file: PDF.js ~ line 5 ~ PDF ~ iframe", iframe)
   }
   return (
-    <div id="container">
-      <iframe onLoad={() => {test()}} id="iframe1" src="https://tailieu.vn/readpdf/tailieu/2022/20220326/vinobelprisen/s12877_021_02392_7_8785.pdf?rand=24059" frameBorder={0} width="100%" height="100%" ></iframe>
-    </div>
+    <iframe onLoad={() => {test()}} id="iframe1" src={`https://homeworkterralogic.herokuapp.com/${props.url}`} frameBorder={0} width="100%" height="100%" ></iframe>
   )
 }
