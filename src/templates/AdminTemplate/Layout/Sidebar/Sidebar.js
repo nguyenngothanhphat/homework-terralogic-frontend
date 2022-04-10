69827cc1ec508b4a3b976a402839f32f05b276a7
Thành Phát <thanhphat19@gmail.com>
2022-04-11 00:49:16 +0700

    update

--- src/templates/AdminTemplate/Layout/Sidebar/Sidebar.js
+++ src/templates/AdminTemplate/Layout/Sidebar/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Sidebar.css'
 
-export default function Sidebar(props) {
+export default function Sidebar() {
   return (
     <aside className="sidebar" id="sidebar">
       <ul className="sidebar-menu">
-        <li className="menu-item"><a href="/"><i className="fas fa-home"></i> Dashboard</a></li>
+        <li className="menu-item"><a href="/admin/dashboard"><i className="fas fa-home"></i> Dashboard</a></li>
       </ul>
     </aside>
   )
